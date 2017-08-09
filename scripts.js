fcc337b67b7eaa17fecb0f7eee810b446582cdc9
Clayton <clayton.collins@eyecuelab.com>
2017-08-09 16:55:08 -0700

    start planes branch

--- scripts.js
+++ scripts.js
@@ -3,20 +3,20 @@ var shapes = [];
 var colors = [];
 
 function preload() {
-  song = loadSound('gogo.m4a');
+  song = loadSound('audio/house no. 3.mp3');
 }
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   song.play();
   fft = new p5.FFT();
   fft.setInput(song);
-  background(135, 206, 250);
+  background(100);
 }
 
 function draw(){
 
-  background(0,0,0);
+  background(100);
   var spectrum = fft.analyze();
   var averages = [];
   var c = 0;
@@ -24,39 +24,31 @@ function draw(){
   for ( i = 0; i<spectrum.length; i++) {
     total += spectrum[i];
     c++;
-    if (c>50) {
+    if (c>10) {
       averages.push(total/50);
       total = 0;
       c = 0;
     }
   }
+  console.log(averages.length)
   var widthpoints = width/averages.length;
 
   var currentcolor = color(150*sin((100+frameCount)/100)+150,150*sin((200+frameCount)/80)+150, 150*sin((300+frameCount)/60)+150);
-  shape = [];
-  colors.push(currentcolor)
-  shape.push([0, height/2]);
+
 
   for (i = 0; i<averages.length; i++) {
-    shape.push([widthpoints+(i*widthpoints*2), map(averages[i], 0, 255, height, 0)]);
-  }
-  shape.push([width, height/2]);
-  shape.push([width, height]);
-  shape.push([0,height]);
-  shapes.push(shape);
-  for (i = shapes.length-1; i>=0; i--) {
+    push();
+    map(averages[i], 0, 255, height, 0)]);
     stroke(255,255,255);
     fill(colors[i]);
     beginShape();
-      for (j = 2; j<shapes[i].length; j++){
-        vertex(shapes[i][j][0], (shapes[i][j][1] - i*10));
-      }
+    for (j = 2; j<shapes[i].length; j++){
+      vertex(shapes[i][j][0], (shapes[i][j][1] - i*10));
+    }
+    vertex(shapes[i][2][0], shapes[i][2][1]);
     endShape();
   }
-  if (shapes.length>50) {
-    shapes.shift();
-    colors.shift();
-  }
+
 
 }
 
